25fd502ed531d5097b0a6427a8f23f041d25a905
Orhan Kupusoglu <orhan.kupusoglu@argela.com.tr>
2020-06-14 20:59:45 +0300

    Add De Bruijn sequence B(2,6) solution, enable with DE_BRUIJN_SEQUENCE

--- src/include/kbtree.h
+++ src/include/kbtree.h
@@ -172,7 +172,11 @@ namespace kupid {
 #if __cplusplus > 201703L  // C++20
                 return std::popcount(bits) == 64;
 #else
+    #ifndef DE_BRUIJN_SEQUENCE
                 return __builtin_popcountll(bits) == 64;
+    #else
+                return bits == 0xFFFFFFFFFFFFFFFF;
+    #endif
 #endif
             }
 
@@ -180,7 +184,25 @@ namespace kupid {
 #if __cplusplus > 201703L  // C++20
                 return std::countr_one(bits);
 #else
+    #ifndef DE_BRUIJN_SEQUENCE
                 return __builtin_ffsll(~bits) - 1;
+    #else
+                static const std::array<uint8_t, 64> debruijn_64 = {
+                     0,  1,  2, 53,  3,  7,  54, 27,  4, 38, 41, 8, 34, 55, 48, 28,
+                    62,  5, 39, 46, 44, 42, 22,  9, 24, 35, 59, 56, 49, 18, 29, 11,
+                    63, 52,  6, 26, 37, 40, 33, 47, 61, 45, 43, 21, 23, 58, 17, 10,
+                    51, 25, 36, 32, 60, 20, 57, 16, 50, 31, 19, 15, 30, 14, 13, 12
+                };
+
+                static const uint64_t debruijn_magic = 0x022FDD63CC95386D;
+
+                bits  = ~bits;
+                bits &= -bits;
+                bits *=  debruijn_magic;
+                uint8_t v = bits >> 58;
+
+                return debruijn_64[v];
+    #endif
 #endif
             }
 
